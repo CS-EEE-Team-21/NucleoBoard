efdc5d8e5c1f14984a29ff9c39d100d46f5e9cb6
juicewar <92922158+juicewa123@users.noreply.github.com>
2023-11-28 01:12:09 +0000

    refactor and beautify
    pretty much the esp32 to nucleoboard communication is done

--- NucleoBoard.ino
+++ NucleoBoard.ino
@@ -6,22 +6,50 @@ const int ESP_TO_NUCLEO_PORT = 9;
 // const int temperatureSensor = ?
 // const int stirringSensor = ?
 
-void printAvailableFromWire(){
-  while (Wire.available()){
-    char c = Wire.read();
-    Serial.print(c);
+///////////////////////////// NUCLEOBOARD FROM ESP32 COMMUNICATION ////////////////////////////////
+
+void receiveEvent(int howMany) {
+  // To send the sensor values, it is transmitted in chunks of bytes
+  char subsystem = Wire.read();
+  byte intByte1 = Wire.read();
+  byte intByte2 = Wire.read();
+  float receivedData = (intByte1 << 8) | intByte2;
+
+  switch (subsystem){
+    case 't':
+      processReceivedTemperature(receivedData);
+      break;
+    case 'p':
+      processReceivedpH(receivedData);
+      break;
+    case 's':
+      processReceivedStirringRPM(receivedData);
+      break;
+    default:
+      Serial.print("Unexpected subsystem '");
+      Serial.print(subsystem);
+      Serial.print("' received. ");
   }
 }
 
-void receiveEvent(int howMany) {
-  Serial.println("\nA message has been received...");
-
-  // To send the sensor values, it is transmitted in chunks of bytes
-  byte byte1 = Wire.read();
-  byte byte2 = Wire.read();
-  int receivedData = (byte1 << 8) | byte2;
-  Serial.println(receivedData);
+void processReceivedTemperature(float temperature){
+  temperature /= 100;
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
 }
+
+void processReceivedpH(float pH){
+  pH /= 100;
+  Serial.print("pH: ");
+  Serial.println(pH);
+}
+
+void processReceivedStirringRPM(float rpm){
+  Serial.print("Motor RPM: ");
+  Serial.println(rpm);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 void requestEvent() {
   Wire.write("hello ");
